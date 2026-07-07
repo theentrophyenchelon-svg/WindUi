af910a87ace664917f28a29fb6f0bb66e04bfb4e
theentrophyenchelon-svg <theentrophyenchelon@gmail.com>
2026-07-07 10:42:37 -0400

    Add main example loader

---
+++ main_example.lua
@@ -0,0 +1,7 @@
+--[[
+    WindUI GodTier Example
+
+    The full example is located in main.client.lua after the source import is completed.
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/theentrophyenchelon-svg/WindUi/main/main.client.lua"))()
